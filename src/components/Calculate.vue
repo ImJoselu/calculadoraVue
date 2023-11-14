f6d52c5b1444fe2755f348c9b3c0dea09bc018b2
Jose Luis <jltortolacer@gmail.com>
2023-11-14 11:57:36 +0100

    Ready to work

---
+++ src/components/Calculate.vue
@@ -0,0 +1,18 @@
+<template lang="">
+    <div>
+        <h1>Hola</h1>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Calculate',
+    data(){
+        return {
+            
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
